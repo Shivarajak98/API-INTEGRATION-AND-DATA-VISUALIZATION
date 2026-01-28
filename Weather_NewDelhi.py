3ce0906bd937a87318e2f55814f6429f5e2bc2a7
Shivarajak98 <shivarajak9985@gmail.com>
2026-01-28 11:41:49 +0530

    Weather_NewDelhi.py

--- Weather_NewDelhi.py
+++ Weather_NewDelhi.py
@@ -8,20 +8,22 @@ import seaborn as sns
 url= "https://api.open-meteo.com/v1/forecast"
 
 params ={
-    "latitude": 28.6139,          # New Delhi, India
-    "longitude": 77.2090,
-    "hourly": "temperature_2m",
-    "timezone": "Asia/Kolkata"
+    "latitude": 28.6139,          # New Delhi , India
+    "longitude": 77.2090 ,
+    "hourly":"temperature_2m" ,
+    "timezone":"Asia/Kolkata"
 }
 response =requests.get(url, params=params)
-data =response.json()
+data=response.json()
 
 # Data Processing
 
-df= pd.DataFrame({
+df= pd.DataFrame(
+{
     "Time":pd.to_datetime(data["hourly"]["time"]),
     "Temperature (°C)":data["hourly"]["temperature_2m"]
-})
+}
+                 )
 
 # Optional:Filter only next 24 hours
 df_24= df.head(24)
@@ -42,17 +44,17 @@ plt.show()
 
 sns.set(style ="whitegrid")
 
-plt.figure (figsize=(12,6))
+plt.figure(figsize=(12,6))
 sns.lineplot (
-    data=df_24,
+    data=df_24 ,
     x="Time",
-    y="Temperature (°C)",
+    y="Temperature (°C)" ,
     marker="o",
     color="red"
 )
-plt.title("Hourly Temperature Change in New Delhi (Next 24 Hours)")
-plt.xlabel("Time (Hourly)")
-plt.ylabel("Temperature (°C)")
-plt.xticks(rotation=45)
+plt.title (" Hourly Temperature Change in New Delhi (Next 24 Hours)")
+plt.xlabel(" Time (Hourly) ")
+plt.ylabel(" Temperature (°C) ")
+plt.xticks (rotation=45)
 plt.tight_layout()
 plt.show()
